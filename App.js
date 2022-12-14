9ba7740686a2a8c61ee6d9d18ac74f893d109245
Murali Elumalai <muralismail4u@gmail.com>
2022-12-14 12:15:22 +0530

    add cashfree

--- App.js
+++ App.js
@@ -1,63 +1,102 @@
+// @ts-nocheck
 import * as React from 'react';
-import Reanimated, {runOnJS} from 'react-native-reanimated';
-
-import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
-import {useCameraDevices, useFrameProcessor} from 'react-native-vision-camera';
-
-import {Camera} from 'react-native-vision-camera';
-import {scanFaces, Face} from 'vision-camera-face-detector';
+import {Component} from 'react';
+import {Button, Platform, StyleSheet, Text, View} from 'react-native';
+import {CFPaymentGatewayService} from 'react-native-cashfree-pg-sdk';
+import {
+  CFDropCheckoutPayment,
+  CFEnvironment,
+  CFPaymentComponentBuilder,
+  CFPaymentModes,
+  CFSession,
+  CFThemeBuilder,
+} from 'cashfree-pg-api-contract';
+const BASE_RESPONSE_TEXT = 'Response or error will show here.';
 
 const styles = StyleSheet.create({
   container: {
-    position: 'relative',
+    padding: Platform.OS === 'ios' ? 56 : 24,
+    backgroundColor: '#eaeaea',
+    alignItems: 'center',
+    flexDirection: 'column',
+    flex: 1,
   },
-
-  captueContainer: {
-    position: 'absolute',
-    bottom: 0,
+  button: {
+    color: '#61aafb',
+    margin: 8,
+    width: 200,
   },
-
-  captureBtn: {
-    backgroundColor: 'red',
+  response_text: {
+    margin: 16,
+    fontSize: 14,
   },
 });
 
 export default function App() {
-  const [hasPermission, setHasPermission] = React.useState(false);
-  const [faces, setFaces] = React.useState();
-
-  const devices = useCameraDevices();
-  const device = devices.front;
+  const [response_text, setResponseText] = React.useState(
+    'Response or error will appear here',
+  );
 
   React.useEffect(() => {
-    console.log(faces);
-  }, [faces]);
+    CFPaymentGatewayService.setCallback({
+      onVerify(orderID) {
+        setResponseText('orderId is :' + orderID);
+      },
+      onError(error, orderID) {
+        setResponseText(
+          'exception is : ' +
+            JSON.stringify(error) +
+            '\norderId is :' +
+            orderID,
+        );
+      },
+    });
 
-  React.useEffect(() => {
-    (async () => {
-      const status = await Camera.requestCameraPermission();
-      setHasPermission(status === 'authorized');
-    })();
-  }, []);
+    return function cleanup() {
+      CFPaymentGatewayService.removeCallback();
+    };
+  });
 
-  const frameProcessor = useFrameProcessor(frame => {
-    'worklet';
-    const scannedFaces = scanFaces(frame);
-    runOnJS(setFaces)(scannedFaces);
-  }, []);
+  let startCheckout = async () => {
+    console.log('Checkout starts here')
+    try {
+      const session = new CFSession(
+        'fuN4XZKQFqBKFw55VsvB',
+        '123123123',
+        CFEnvironment.SANDBOX,
+      );
+      const paymentModes = new CFPaymentComponentBuilder()
+        .add(CFPaymentModes.CARD)
+        .add(CFPaymentModes.UPI)
+        .add(CFPaymentModes.NB)
+        .add(CFPaymentModes.WALLET)
+        .add(CFPaymentModes.PAY_LATER)
+        .build();
+      const theme = new CFThemeBuilder()
+        .setNavigationBarBackgroundColor('#E64A19')
+        .setNavigationBarTextColor('#FFFFFF')
+        .setButtonBackgroundColor('#FFC107')
+        .setButtonTextColor('#FFFFFF')
+        .setPrimaryTextColor('#212121')
+        .setSecondaryTextColor('#757575')
+        .build();
+      const dropPayment = new CFDropCheckoutPayment(
+        session,
+        paymentModes,
+        theme,
+      );
+      CFPaymentGatewayService.doPayment(dropPayment);
+    } catch (e) {
+      console.log(e.message);
+    }
+  };
 
-  return device != null && hasPermission ? (
+  return (
     <View style={styles.container}>
-      <Camera
-        style={StyleSheet.absoluteFill}
-        device={device}
-        isActive={true}
-        frameProcessor={frameProcessor}
-        frameProcessorFps={1 / 2}
-      />
-      <TouchableOpacity style={styles.captureBtn} onPress={this.takePicture}>
-          <Text>Take Photo</Text>
-        </TouchableOpacity>
+      <View style={styles.button}>
+        <Button onPress={() => startCheckout()} title="Start payment" />
+        <Text style={styles.response_text}>{response_text}</Text>
+      </View>
     </View>
-  ) : null;
+  );
 }
